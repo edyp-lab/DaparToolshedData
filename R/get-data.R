18582e0c6a78a0205718dfd8362119c31455b3f6
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2025-10-10 16:05:13 +0200

    Update

--- R/get-data.R
+++ R/get-data.R
@@ -4,16 +4,16 @@
 #' 
 #' @examples
 #' library(ExperimentHub)
-#' eh = ExperimentHub()
-#' object <- query(eh, "DaparToolshedData")
-#' 
 #' GetData(object, 'Exp1_R25_prot')
 #' 
 #' @name access-functions
-#' 
+#' @importFrom ExperimentHub ExperimentHub
+#' @importFrom AnnotationHub query
+#' @importFrom S4Vectors mcols
 #' @export
-GetData <- function(object, name) {
-
+GetData <- function(name) {
+  eh = ExperimentHub()
+  object <- query(eh, "DaparToolshedData")
   ind <- which(mcols(object)$title == name)
   .name <- rownames(mcols(object))[ind]
   load(object[[.name]])
